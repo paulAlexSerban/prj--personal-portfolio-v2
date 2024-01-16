f4c666bee85cc738187679328ce4dae1492b577a
paulAlexSerban <paul.alex.serban@gmail.com>
2024-01-16 02:33:48 +0200

    feat(heading): added in storybok

---
+++ frontend/ds-storybook/src/atoms/Heading/Heading.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Heading } from '@prj--personal-portfolio-v2/react-cmp-lib';
+import '@prj--personal-portfolio-v2/ds-lsg-styles/lib/components/atoms/heading.css';
+
+const meta = {
+    title: 'Atoms/Heading',
+    component: Heading,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        title: {
+            control: {
+                type: 'text',
+            },
+        },
+        level: {
+            control: {
+                type: 'select',
+            },
+            options: [1, 2, 3, 4, 5, 6],
+        },
+        align: {
+            control: {
+                type: 'select',
+            },
+            options: ['left', 'center', 'right'],
+        },
+        subtitle: {
+            control: {
+                type: 'text',
+            },
+        },
+    },
+} satisfies Meta<typeof Heading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        title: 'Hello World',
+        level: 1,
+        align: 'center',
+    },
+};
