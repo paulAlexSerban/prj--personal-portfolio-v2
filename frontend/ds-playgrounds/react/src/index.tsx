473a8e6d270993f3bf376b511a598ac5bdb66c32
paulAlexSerban <paul.alex.serban@gmail.com>
2024-01-23 01:05:17 +0200

    feat(global styles): added

--- frontend/ds-playgrounds/react/src/index.tsx
+++ frontend/ds-playgrounds/react/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
+import { createRoot } from 'react-dom/client';
 import { Paragraph } from '@prj--personal-portfolio-v2/react-cmp-lib/lib/index';
+
+import '@prj--personal-portfolio-v2/ds-lsg-styles/lib/system/pages/main.css';
 import paragraphStyles from '@prj--personal-portfolio-v2/ds-lsg-styles/lib/components/atoms/paragraph.module.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
+root.render(
     <>
         <Paragraph styles={paragraphStyles} size="lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. At, accusantium?
         </Paragraph>
-    </>,
-    document.querySelector('#root')
+    </>
 );
